1b9be739d852c2e71603fa7b2cd000cd4903429e
Peter St. John <peterc.stjohn@gmail.com>
2021-12-15 09:52:47 -0700

    refactoring for tf-serving

--- api.py
+++ api.py
@@ -1,21 +1,25 @@
-import json
-from typing import List, Optional, Dict
 import logging
+from typing import Dict, List, Optional, Tuple
+
+import numpy as np
 import pandas as pd
-from alfabet.drawing import draw_bde, draw_mol_outlier, draw_mol
-from alfabet.fragment import get_fragments, canonicalize_smiles
+from alfabet.drawing import draw_bde, draw_mol, draw_mol_outlier
+from alfabet.fragment import canonicalize_smiles, get_fragments
 from alfabet.neighbors import get_neighbors
-from alfabet.prediction import predict_bdes, validate_inputs
+from alfabet.prediction import (
+    format_predictions_into_dataframe,
+    tf_model_forward,
+    validate_inputs,
+)
 from alfabet.preprocessor import get_features
-from fastapi import FastAPI, HTTPException, Depends, Response
+from fastapi import Depends, FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 api = FastAPI()
-api.add_middleware(CORSMiddleware,
-                   allow_origins=["*"],
-                   allow_methods=["*"],
-                   allow_headers=["*"])
+api.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
 logger = logging.getLogger(__name__)
 
 
@@ -83,57 +87,90 @@ async def canonicalize(smiles: str):
 
 @api.get("/fragment/{smiles}", response_model=List[Bond])
 async def fragment(smiles: str = Depends(canonicalize)):
-    return get_fragments(smiles).to_dict(orient='records')
+    return get_fragments(smiles).to_dict(orient="records")
 
 
 @api.get("/featurize/{smiles}", response_model=Features)
-async def featurize(smiles: str = Depends(canonicalize)):
-    return {key: val.tolist() for key, val in get_features(smiles).items()}
+async def featurize(smiles: str = Depends(canonicalize), pad: bool = True):
+    return {key: val.tolist() for key, val in get_features(smiles, pad).items()}
 
 
-@api.get("/validate/{smiles}", response_model=Features,
-         responses={400: {"model": Outlier}})
-async def validate(
-        smiles: str,
-        features: Features = Depends(featurize)):
+@api.get(
+    "/validate/{smiles}", response_model=Features, responses={400: {"model": Outlier}}
+)
+async def validate(smiles: str, features: Features = Depends(featurize)):
     is_outlier, missing_atom, missing_bond = validate_inputs(dict(features))
     if is_outlier:
-        raise HTTPException(status_code=400,
-                            detail={
-                                "is_valid": False,
-                                "SMILES": smiles,
-                                "missing_atom": missing_atom.tolist(),
-                                "missing_bond": missing_bond.tolist(),
-                            })
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "is_valid": False,
+                "SMILES": smiles,
+                "missing_atom": missing_atom.tolist(),
+                "missing_bond": missing_bond.tolist(),
+            },
+        )
     else:
         features_out = dict(features)
-        features_out['is_valid'] = True
+        features_out["is_valid"] = True
         return features_out
+
+
+def predict_bdes(
+    features: Features = Depends(validate),
+) -> Tuple[List[float], List[float]]:
+    """This function should get replaced with the tf-serving CURL. Ideally this would
+    also get called as a coroutine, since we'd get a lot of speedup by not blocking for
+    each TFServing call
+
+    Args:
+        features (Features, optional): the input dictionary to the TF model
+
+    Returns:
+        Tuple[List[float], List[float]]: Predicted BDEs and BDFEs as list of floats
+    """
+    return tf_model_forward(features)
 
 
 @api.get("/predict/{smiles}/{bond_index}", response_model=BondPrediction)
 @api.get("/predict/{smiles}", response_model=List[BondPrediction])
-async def predict(fragments: List[Bond] = Depends(fragment),
-                  features: Features = Depends(validate),
-                  drop_duplicates: bool = False,
-                  bond_index: Optional[int] = None):
+async def predict(
+    fragments: List[Bond] = Depends(fragment),
+    features: Features = Depends(validate),
+    drop_duplicates: bool = False,
+    bond_index: Optional[int] = None,
+):
+
     features = dict(features)
-    features.pop('is_valid')
+    features.pop("is_valid")
+    bde, bdfe = predict_bdes(features)
+
     fragments = pd.DataFrame.from_records(fragments)
-    bde_pred = predict_bdes(fragments, features, drop_duplicates=drop_duplicates)
+    bde_pred = format_predictions_into_dataframe(
+        bde, bdfe, fragments, drop_duplicates=drop_duplicates
+    )
+
     if bond_index is not None:
-        solo_bde = bde_pred[bde_pred['bond_index'] == bond_index]
+        solo_bde = bde_pred[bde_pred["bond_index"] == bond_index]
         return pandas_to_records(solo_bde)[0]
     return pandas_to_records(bde_pred)
 
 
-@api.get("/draw/{smiles}/{bond_index}", response_class=Response,
-         responses={200: {"content": {"image/svg+xml": {}}},
-                    400: {"model": Message}})
-@api.get("/draw/{smiles}", response_class=Response, responses={200: {"content": {"image/svg+xml": {}}}})
-async def draw(smiles: str = Depends(canonicalize),
-               features: Features = Depends(featurize),
-               bond_index: Optional[int] = None):
+@api.get(
+    "/draw/{smiles}/{bond_index}",
+    response_class=Response,
+    responses={200: {"content": {"image/svg+xml": {}}}, 400: {"model": Message}},
+)
+@api.get(
+    "/draw/{smiles}",
+    response_class=Response,
+    responses={200: {"content": {"image/svg+xml": {}}}},
+)
+async def draw(
+    smiles: str = Depends(canonicalize),
+    features: Features = Depends(featurize),
+    bond_index: Optional[int] = None,
+):
     if bond_index is not None:
         try:
             svg = draw_bde(smiles, bond_index)
@@ -148,12 +185,15 @@ async def draw(smiles: str = Depends(canonicalize),
     return Response(content=svg, media_type="image/svg+xml")
 
 
-@api.get("/neighbors/{smiles}/{bond_index}", response_model=List[Neighbor],
-         responses={400: {"model": Message}})
+@api.get(
+    "/neighbors/{smiles}/{bond_index}",
+    response_model=List[Neighbor],
+    responses={400: {"model": Message}},
+)
 async def neighbors(bond_index: int, features: Features = Depends(validate)):
     features = dict(features)
-    features.pop('is_valid')
-    if bond_index in features['bond_indices']:
+    features.pop("is_valid")
+    if bond_index in features["bond_indices"]:
         return pandas_to_records(get_neighbors(features, bond_index))
     else:
         raise HTTPException(status_code=400, detail="Invalid bond index")
--- tests/test_api.py
+++ tests/test_api.py
@@ -11,83 +11,91 @@ fastapi_client = TestClient(api)
 
 
 def test_canonicalize():
-    response = fastapi_client.get('/canonicalize/C1=CC=CC=C1')
+    response = fastapi_client.get("/canonicalize/C1=CC=CC=C1")
     assert response.status_code == 200
-    assert response.json() == 'c1ccccc1'
+    assert response.json() == "c1ccccc1"
 
-    response = fastapi_client.get('/canonicalize/C1')
+    response = fastapi_client.get("/canonicalize/C1")
     assert response.status_code == 400
 
-    response = fastapi_client.get('/canonicalize/')
+    response = fastapi_client.get("/canonicalize/")
     assert response.status_code == 404
 
 
 def test_fragment():
-    response = fastapi_client.get('/fragment/C1=CC=CC=C1')
+    response = fastapi_client.get("/fragment/C1=CC=CC=C1")
     assert response.status_code == 200
     ret = pandas.read_json(response.text)
-    desired = pandas.read_json(os.path.join(os.path.dirname(__file__), "benzene_frags.json"))
+    desired = pandas.read_json(
+        os.path.join(os.path.dirname(__file__), "benzene_frags.json")
+    )
     pandas.testing.assert_frame_equal(desired, ret, check_exact=False)
 
 
 def test_featurize():
-    response = fastapi_client.get('/featurize/C1=CC=CC=C1')
+    response = fastapi_client.get("/featurize/C1=CC=CC=C1")
     assert response.status_code == 200
     data = response.json()
-    assert set(data.keys()) == {'atom', 'bond', 'bond_indices', 'connectivity', 'is_valid'}
+    assert set(data.keys()) == {
+        "atom",
+        "bond",
+        "bond_indices",
+        "connectivity",
+        "is_valid",
+    }
 
 
 def test_validate():
-    response = fastapi_client.get('/validate/CB')
+    response = fastapi_client.get("/validate/CB")
     assert response.status_code == 400
     data = response.json()
-    assert not data['detail']['is_valid']
+    assert not data["detail"]["is_valid"]
 
-    response = fastapi_client.get('/validate/C1=CC=CC=C1')
+    response = fastapi_client.get("/validate/C1=CC=CC=C1")
     assert response.status_code == 200
     data = response.json()
-    assert data['is_valid']
+    assert data["is_valid"]
 
 
 def test_predict():
-    response = fastapi_client.get('/predict/C1=CC=CC=C1?drop_duplicates=True')
+    response = fastapi_client.get("/predict/C1=CC=CC=C1?drop_duplicates=True")
     assert response.status_code == 200
     ret = pandas.read_json(response.text)
-    assert np.isclose(ret.bde, 110.9, rtol=1E-3)
-    assert np.isclose(ret.bde_pred, 110.9, rtol=1E-2)
+    assert np.isclose(ret.bde, 110.9, rtol=1e-3)
+    assert np.isclose(ret.bde_pred, 110.9, rtol=1e-2)
     assert len(ret) == 1
 
-    response = fastapi_client.get('/predict/C1=CC=CC=C1?drop_duplicates=False')
+    response = fastapi_client.get("/predict/C1=CC=CC=C1?drop_duplicates=False")
     assert response.status_code == 200
     ret = pandas.read_json(response.text)
     assert len(ret) == 6
 
     # we don't have DFT values for all bonds in this molecule
-    response = fastapi_client.get('/predict/CC(C)CC1CCCCC1?drop_duplicates=True')
+    response = fastapi_client.get("/predict/CC(C)CC1CCCCC1?drop_duplicates=True")
     assert response.status_code == 200
     ret = pandas.read_json(response.text)
     assert len(ret) == 10
-    assert len(ret.dropna(subset=['bde'])) == 7
+    assert len(ret.dropna(subset=["bde"])) == 7
 
 
 def test_draw():
-    response = fastapi_client.get('/draw/C1=CC=CC=C1/6')
+    response = fastapi_client.get("/draw/C1=CC=CC=C1/6")
     assert response.status_code == 200
 
-    response = fastapi_client.get('/draw/CC/10')
+    response = fastapi_client.get("/draw/CC/10")
     assert response.status_code == 400
 
 
 def test_neighbors():
-    response = fastapi_client.get('/neighbors/CC/0')
+    response = fastapi_client.get("/neighbors/CC/0")
     assert response.status_code == 200
     neighbor_df = pandas.read_json(response.text)
     for _, row in neighbor_df.iterrows():
         mol = rdkit.Chem.AddHs(rdkit.Chem.MolFromSmiles(row.molecule))
         bond = mol.GetBondWithIdx(row.bond_index)
-        assert bond.GetEndAtom().GetSymbol() == 'C'
-        assert bond.GetBeginAtom().GetSymbol() == 'C'
+        assert bond.GetEndAtom().GetSymbol() == "C"
+        assert bond.GetBeginAtom().GetSymbol() == "C"
         assert bond.GetBondType() == rdkit.Chem.rdchem.BondType.SINGLE
 
-    response = fastapi_client.get('/neighbors/CC/10')
+    response = fastapi_client.get("/neighbors/CC/10")
     assert response.status_code == 400
